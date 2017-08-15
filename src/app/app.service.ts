15fc9410b16eef2a2a603001c5bc149dec69ae58
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-08-15 03:46:25 -0500

    udating files

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-
+/* 
+    Instalar ts-node
+    Instalar Lodash
+    Instalar Express
+    instalar Body Parser 
+*/
 @Injectable()
 export class AppService {
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http) {
+
+   }
 
 }
